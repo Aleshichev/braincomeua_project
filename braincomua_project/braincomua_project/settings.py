56b143657653029dd2b9c44198454a2593cdf1d7
Igor <Aleshichev.git@email.ua>
2025-11-20 16:05:23 +0200

    fetc: add psycopg2-binary

--- braincomua_project/braincomua_project/settings.py
+++ braincomua_project/braincomua_project/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("POSTGRES_DB"),
-        "USER": os("POSTGRES_USER"),
+        "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": os.getenv("DB_HOST"),
         "PORT": os.getenv("DB_PORT"),
